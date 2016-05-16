dfb1ca0bdde0f7b4032d25094c8610a6574447f9
umerror <mumernawaz@gmail.com>
2016-05-16 17:12:27 +0500

    Added associations bwuser and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   end
 
   def new
-  	@post = Post.new
+  	@post = current_user.posts.build
   end  
 
   def show
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
   end
 
   def create
-  	@post = Post.new(permit_post)
+  	@post = current_user.posts.build(permit_post)
   	if @post.save
   		flash[:success] = "Success!"
   		redirect_to post_path(@post) 
